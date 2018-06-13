57eb84a4e4c1259083ec21331485922e20a30417
zhengxianda <xianda.zheng@jiamiantech.com>
2018-06-13 11:18:36 +0800

    规范化案例展示方式

---
+++ OneWayDemo/ToDoViewController.swift
@@ -0,0 +1,130 @@
+//
+//  ToDoViewController.swift
+//  OneWayDemo
+//
+//  Created by zhengxianda on 2018/4/11.
+//  Copyright © 2018年 Toki. All rights reserved.
+//
+
+import UIKit
+
+class ToDoViewController: UIViewController {
+
+    //MARK: - 状态
+    struct State: StateType {
+        var toDoList: [ToDo] = []
+        var sectionCount: Int = 0
+    }
+    
+    //MARK: - 事件
+    enum Action: ActionType {
+        case queryToDoList
+        case toDoTableView(action: ToDoTableView.Action)
+    }
+    
+    //MARK: - 关联 事件、状态变化、异步命令
+    lazy var reducer: (Action, State) -> State = { [unowned self] (action: Action, state: State) in
+        
+        var state:State = state
+        
+        switch action {
+        case .queryToDoList:
+            ToDoDataManager.shared.getToDoItems(completionHandler: { (toDoList) in
+                self.store.dispatch(.toDoTableView(action: .updateToDoList(toDoList: toDoList)))
+            })
+        case .toDoTableView(let action):
+            self.toDoTableView.store.dispatch(action)
+        }
+        return state
+    }
+    
+    //MARK: - 仓库
+    var store: TKStore<Action, State>!
+    
+    //用户界面属性
+    lazy var toDoTableView: ToDoTableView = {
+        let temp = ToDoTableView()
+        return temp
+    }()
+    
+    //生命周期
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        setupUI()
+        setupUIResponse()
+        setupStore()
+        
+        DispatchQueue.main.asyncAfter(deadline: .now() + 0.5) {
+            self.store.dispatch(.queryToDoList)
+        }
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+    //MARK: - 处理状态变化
+    func stateDidChanged(state: State, previousState: State?) {
+        
+    }
+}
+
+//MARK: - 代理事件
+extension ToDoViewController: ToDoTableViewDelegate {
+    func toDoTableView(_ toDoTableView: ToDoTableView, toDoTableViewCell: ToDoTableViewCell, toDoView: ToDoView, statusBoxView: StatusBoxView, statusButtonTapped statusButton: UIButton, with status: ToDo.Status) {
+//        switch status {
+//        case .none:
+//            break
+//        case .doing:
+//            statusBoxView.store.dispatch(.updateStatus(status: .completed))
+//        case .completed:
+//            statusBoxView.store.dispatch(.updateStatus(status: .doing))
+//        }
+    }
+}
+
+
+//MARK: - 用户界面相关配置
+extension ToDoViewController {
+    
+    func setupUI() {
+        toDoTableView.agent = self
+        toDoTableView.allowsSelection = false
+        view.addSubview(toDoTableView)
+        toDoTableView.snp.makeConstraints { (make) in
+            make.edges.equalToSuperview()
+        }
+    }
+    
+    func setupUIResponse() {
+        
+    }
+}
+
+//MARK: - 仓库相关配置
+extension ToDoViewController {
+    func setupStore() {
+        store = TKStore<Action, State>(reducer: reducer, initialState: State())
+        store.subscribe { [unowned self] state, previousState in
+            self.stateDidChanged(state: state, previousState: previousState)
+        }
+        
+        store.dispatch(.toDoTableView(action:
+            .updateToDoCellEvent(cellEvent:
+              .toDoView(action:
+                .statusBoxView(action:
+                    .updateChangeStatusEvent(changeStatusEvent: { (statusBoxView, status) in
+                        switch status {
+                        case .none:
+                            break
+                        case .doing:
+                            statusBoxView.store.dispatch(.updateStatus(status: .completed))
+                        case .completed:
+                            statusBoxView.store.dispatch(.updateStatus(status: .doing))
+                        }
+                    }))))))
+        
+        stateDidChanged(state: store.state, previousState: nil)
+    }
+}
--- OneWayDemo/ViewController.swift
+++ OneWayDemo/ViewController.swift
@@ -9,122 +9,30 @@
 import UIKit
 
 class ViewController: UIViewController {
-
-    //MARK: - 状态
-    struct State: StateType {
-        var toDoList: [ToDo] = []
-        var sectionCount: Int = 0
-    }
     
-    //MARK: - 事件
-    enum Action: ActionType {
-        case queryToDoList
-        case toDoTableView(action: ToDoTableView.Action)
-    }
-    
-    //MARK: - 关联 事件、状态变化、异步命令
-    lazy var reducer: (Action, State) -> State = { [unowned self] (action: Action, state: State) in
-        
-        var state:State = state
-        
-        switch action {
-        case .queryToDoList:
-            ToDoDataManager.shared.getToDoItems(completionHandler: { (toDoList) in
-                self.store.dispatch(.toDoTableView(action: .updateToDoList(toDoList: toDoList)))
-            })
-        case .toDoTableView(let action):
-            self.toDoTableView.store.dispatch(action)
-        }
-        return state
-    }
-    
-    //MARK: - 仓库
-    var store: TKStore<Action, State>!
-    
-    //用户界面属性
-    lazy var toDoTableView: ToDoTableView = {
-        let temp = ToDoTableView()
+    lazy var actionButton : UIButton = {
+        let temp = UIButton()
         return temp
     }()
     
-    //生命周期
     override func viewDidLoad() {
         super.viewDidLoad()
-        setupUI()
-        setupUIResponse()
-        setupStore()
         
-        DispatchQueue.main.asyncAfter(deadline: .now() + 0.5) {
-            self.store.dispatch(.queryToDoList)
+        actionButton.backgroundColor = UIColor.gray
+        actionButton.setTitle("Action", for: UIControlState.normal)
+        actionButton.setTitleColor(UIColor.white, for: UIControlState.normal)
+        view.addSubview(actionButton)
+        actionButton.snp.makeConstraints { (make) in
+            make.center.equalToSuperview()
+            make.size.equalTo(CGSize(width: 100, height: 100))
         }
-    }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
-
-    //MARK: - 处理状态变化
-    func stateDidChanged(state: State, previousState: State?) {
         
-    }
-}
-
-//MARK: - 代理事件
-extension ViewController: ToDoTableViewDelegate {
-    func toDoTableView(_ toDoTableView: ToDoTableView, toDoTableViewCell: ToDoTableViewCell, toDoView: ToDoView, statusBoxView: StatusBoxView, statusButtonTapped statusButton: UIButton, with status: ToDo.Status) {
-//        switch status {
-//        case .none:
-//            break
-//        case .doing:
-//            statusBoxView.store.dispatch(.updateStatus(status: .completed))
-//        case .completed:
-//            statusBoxView.store.dispatch(.updateStatus(status: .doing))
-//        }
-    }
-}
-
-
-//MARK: - 用户界面相关配置
-extension ViewController {
-    
-    func setupUI() {
-        toDoTableView.agent = self
-        toDoTableView.allowsSelection = false
-        view.addSubview(toDoTableView)
-        toDoTableView.snp.makeConstraints { (make) in
-            make.edges.equalToSuperview()
-        }
+        actionButton.addTarget(self, action: #selector(action), for: UIControlEvents.touchUpInside)
     }
     
-    func setupUIResponse() {
-        
+    @objc func action() {
+       navigationController?.pushViewController(ToDoViewController(), animated: true)
     }
+    
 }
 
-//MARK: - 仓库相关配置
-extension ViewController {
-    func setupStore() {
-        store = TKStore<Action, State>(reducer: reducer, initialState: State())
-        store.subscribe { [unowned self] state, previousState in
-            self.stateDidChanged(state: state, previousState: previousState)
-        }
-        
-        store.dispatch(.toDoTableView(action:
-            .updateToDoCellEvent(cellEvent:
-              .toDoView(action:
-                .statusBoxView(action:
-                    .updateChangeStatusEvent(changeStatusEvent: { (statusBoxView, status) in
-                        switch status {
-                        case .none:
-                            break
-                        case .doing:
-                            statusBoxView.store.dispatch(.updateStatus(status: .completed))
-                        case .completed:
-                            statusBoxView.store.dispatch(.updateStatus(status: .doing))
-                        }
-                    }))))))
-        
-        stateDidChanged(state: store.state, previousState: nil)
-    }
-}
